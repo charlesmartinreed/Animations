560dc9c7b89bd1e3aee88042ff2abe429486902f
Charles Martin Reed <charlesmartinreed@icloud.com>
2018-09-14 15:36:08 -0500

    Working version. Tap the button to progress the animation.

--- Animations/ViewController.swift
+++ Animations/ViewController.swift
@@ -9,12 +9,32 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    //MARK:- Properties
+    var imageCount = 1
 
+    @IBOutlet weak var imageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        imageView.image = UIImage(named: "frame\(imageCount).gif")
     }
 
-
+    @IBAction func nextFrame(_ sender: Any) {
+        //loop through all the images
+        
+        imageCount += 1
+            
+        imageView.image = UIImage(named: "frame\(imageCount).gif")
+        
+        if imageCount == 10 {
+            imageCount = 1
+            
+            imageView.image = UIImage(named: "frame\(imageCount).gif")
+        }
+        
+        
+    }
+    
 }
 
